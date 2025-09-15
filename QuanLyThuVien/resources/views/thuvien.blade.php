301a8c0960ccaeae5cab41bef06121cc19f5ff04
Le Thu Nguyen <10757059+nglthu@users.noreply.github.com>
2025-09-15 01:08:27 +0000

    example

--- QuanLyThuVien/resources/views/thuvien.blade.php
+++ QuanLyThuVien/resources/views/thuvien.blade.php
@@ -1,11 +1,13 @@
-<div>
-    <p>{{$name[0][0]}} </p>
-    <p>{{$name[0][1]}} </p>
-    <p>{{$name[0][2]}} chao tat ca moi nguoi</p>
+<div class="row">
 
+    @for($i = 0; $i < 2; $i++)
+        <div class="row">
+            @for($j = 0; $j < 2; $j++)
+                <div class="col">
+                    {{$name[$i][$j]}}
+                </div>
+            @endfor
+        </div>
+    @endfor
 
-    <p>=============================================</p>
-     <p>{{$name[1][0]}} </p>
-    <p>{{$name[1][1]}} </p>
-    <p>{{$name[1][2]}} chao tat ca moi nguoi</p>
 </div>
